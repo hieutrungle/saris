ae4c9058ea1edf86ba2f7d56408070e71daf1bc9
HieuLe <hieult1996@gmail.com>
2024-08-22 19:06:19 -0500

    trainers

--- saris/cli.py
+++ saris/cli.py
@@ -3,19 +3,47 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"  # to avoid memory fragmentation
 # os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".30"
 
 # Jax acceleration flags
 os.environ["XLA_FLAGS"] = (
     "--xla_gpu_enable_triton_softmax_fusion=true "
     "--xla_gpu_triton_gemm_any=True "
-    "--xla_gpu_enable_async_collectives=true "
-    "--xla_gpu_enable_latency_hiding_scheduler=true "
-    "--xla_gpu_enable_highest_priority_async_stream=true "
+    # "--xla_gpu_enable_async_collectives=true "
+    # "--xla_gpu_enable_latency_hiding_scheduler=true "
+    # "--xla_gpu_enable_highest_priority_async_stream=true "
 )
 
 import argparse
 from saris.utils import utils
 import numpy as np
+import gymnasium as gym
+from saris.drl.envs import register_envs
+from saris.drl.networks import actor, critic
+import jax
+
+
+def make_env(
+    env_id, sionna_config_file, drl_config_file, idx, capture_video=None, run_name=None
+):
+    drl_config = utils.load_yaml_file(drl_config_file)
+    seed = drl_config["seed"]
+
+    def thunk():
+        env = gym.make(env_id, sionna_config_file=sionna_config_file, seed=seed)
+        env = gym.wrappers.TimeLimit(env, max_episode_steps=drl_config["ep_len"])
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if run_name is None:
+                run_name = f"{env_id}_seed_{seed}_idx_{idx}"
+            else:
+                if idx == 0:
+                    env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+
+    return thunk
 
 
 def main():
@@ -29,6 +57,79 @@ def main():
         utils.log_args(args)
         utils.log_config(drl_config)
         utils.log_config(sionna_config)
+
+    # Env
+    register_envs()
+    env = make_env(
+        env_id=drl_config["env_id"],
+        sionna_config_file=args.sionna_config_file,
+        drl_config_file=args.drl_config_file,
+        idx=0,
+    )()
+    discrete = isinstance(env.action_space, gym.spaces.Discrete)
+    assert (
+        not discrete
+    ), "Our wireless DRL implementation only supports continuous action spaces."
+
+    print(env)
+    ob_space = env.observation_space
+    ac_space = env.action_space
+    print(f"Observation space: {ob_space}")
+    print(f"Action space: {ac_space}")
+
+    # Create AC agent
+    key = jax.random.PRNGKey(args.seed)
+    actor_model = actor.Actor.create(
+        num_actions=ac_space.shape[0],
+        features=drl_config["hidden_sizes"],
+        activation="tanh",
+        dtype="bfloat16",
+    )
+    critic_model = critic.Crtic.create(
+        features=drl_config["hidden_sizes"],
+        activation="relu",
+        dtype="bfloat16",
+    )
+    actor_example_inputs = np.zeros(ob_space.shape, dtype=np.float32)
+    critic_example_inputs = [
+        np.zeros(ob_space.shape, dtype=np.float32),
+        np.zeros(ac_space.shape, dtype=np.float32),
+    ]
+    print(actor_model.tabulate(key, actor_example_inputs))
+    print(critic_model.tabulate(key, *critic_example_inputs))
+
+    # for i in range(1):
+    #     ob, info = env.reset()
+    #     done = False
+    #     j = 0
+    #     while not done:
+    #         ac = ac_space.sample()
+    #         ob, rew, terminated, truncated, info = env.step(ac)
+    #         print(
+    #             f"step={j}, reward={rew}, truncated={truncated}, terminated={terminated}, info={info}"
+    #         )
+    #         if "episode" in info.keys():
+    #             print(f"global_step={j}, episodic_return={info['episode']['r']}")
+    #         done = terminated or truncated
+    #         if done or j >= 300:
+    #             break
+
+    if args.command == "train":
+
+        # assets_dir = utils.get_asset_dir()
+        # replay_buffer_dir = os.path.join(assets_dir, "replay_buffer")
+        # buffer_saved_dir = os.path.join(replay_buffer_dir, drl_config.log_name)
+        # utils.mkdir_not_exists(buffer_saved_dir)
+        # replay_buffer = WirelessReplayBuffer(
+        #     drl_config.replay_buffer_capacity, buffer_saved_dir, seed=seed
+        # )
+
+        # run_training_loop(drl_config, tsb_logger, args, env, agent, replay_buffer)
+        pass
+    elif args.command == "eval":
+        pass
+    else:
+        raise ValueError(f"Invalid command: {args.command}")
 
 
 def parse_agrs():
---
+++ saris/drl/trainers/actor_trainer.py
@@ -0,0 +1,105 @@
+from typing import Any, Tuple, Callable
+import jax
+from jax import numpy as jnp
+from saris.drl.trainers import trainer_module
+
+# Type aliases
+PyTree = Any
+
+
+class ActorTrainer(trainer_module.TrainerModule):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def create_step_functions(self):
+
+        def actor_loss(
+            actor_params: PyTree,
+            actor_apply_fn: Callable,
+            critic_states: Tuple[trainer_module.TrainState],
+            target_critic_states: Tuple[trainer_module.TrainState],
+            batch: Tuple[jnp.ndarray],
+            train: bool,
+            rng_key: jax.random.PRNGKey,
+        ):
+            obs, acts, rews, next_obs, dones = batch
+            # pred = self.model.apply(
+            #     {"params": actor_params}, x, train=train, rngs={"dropout": rng_key}
+            # )
+            # axes = tuple(range(1, len(y.shape)))
+            # loss = jnp.sum(jnp.mean((pred - y) ** 2, axis=axes))
+            loss = 0
+            return loss
+
+        def accumulate_gradients(
+            actor_state, critic_states, target_critic_states, batch, rng_key
+        ):
+            batch_size = batch[0].shape[0]
+            num_minibatches = self.grad_accum_steps
+            minibatch_size = batch_size // self.grad_accum_steps
+            rngs = jax.random.split(rng_key, num_minibatches)
+            grad_fn = jax.value_and_grad(actor_loss)
+
+            def _minibatch_step(
+                minibatch_idx: jax.Array | int,
+            ) -> Tuple[PyTree, jnp.ndarray]:
+                """Determine gradients and metrics for a single minibatch."""
+                minibatch = jax.tree_map(
+                    lambda x: jax.lax.dynamic_slice_in_dim(  # Slicing with variable index (jax.Array).
+                        x,
+                        start_index=minibatch_idx * minibatch_size,
+                        slice_size=minibatch_size,
+                        axis=0,
+                    ),
+                    batch,
+                )
+                step_loss, step_grads = grad_fn(
+                    actor_state.params,
+                    minibatch,
+                    train=True,
+                    rng_key=rngs[minibatch_idx],
+                )
+                return step_loss, step_grads
+
+            def _scan_step(
+                carry: Tuple[PyTree, jnp.ndarray], minibatch_idx: jax.Array | int
+            ) -> Tuple[Tuple[PyTree, jnp.ndarray], None]:
+                """Scan step function for looping over minibatches."""
+                step_loss, step_grads = _minibatch_step(minibatch_idx)
+                carry = jax.tree_map(jnp.add, carry, (step_loss, step_grads))
+                return carry, None
+
+            # Determine initial shapes for gradients and loss.
+            loss_shape, grads_shapes = jax.eval_shape(_minibatch_step, 0)
+            grads = jax.tree_map(lambda x: jnp.zeros(x.shape, x.dtype), grads_shapes)
+            loss = jax.tree_map(lambda x: jnp.zeros(x.shape, x.dtype), loss_shape)
+
+            # Loop over minibatches to determine gradients and metrics.
+            (loss, grads), _ = jax.lax.scan(
+                _scan_step,
+                init=(loss, grads),
+                xs=jnp.arange(num_minibatches),
+                length=num_minibatches,
+            )
+
+            # Average gradients over minibatches.
+            grads = jax.tree_map(lambda g: g / num_minibatches, grads)
+            return loss, grads
+
+        def train_step(actor_state, critic_states, target_critic_states, batch):
+            rng, step_rng = jax.random.split(actor_state.rng)
+            loss, grads = accumulate_gradients(
+                actor_state, critic_states, target_critic_states, batch, step_rng
+            )
+            actor_state = actor_state.apply_gradients(grads=grads, rng=rng)
+            metrics = {"loss": loss}
+            return actor_state, metrics
+
+        def eval_step(actor_state, critic_states, target_critic_states, batch):
+            loss = actor_loss(
+                actor_state.params, batch, train=False, rng_key=actor_state.rng
+            )
+            return {"loss": loss}
+
+        return train_step, eval_step
