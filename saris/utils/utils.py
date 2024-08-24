aad3640d656bb61bc308f969a51fa40bf567d260
hieule <hieult1996@gmail.com>
2024-08-24 15:48:01 -0500

    add load_config

--- saris/utils/utils.py
+++ saris/utils/utils.py
@@ -180,6 +180,23 @@ def write_yaml_file(file_path: str, data: dict) -> None:
     os.rename(tmp_file, file_path)
 
 
+def load_config(config_file: str) -> dict:
+    config_kwargs = load_yaml_file(config_file)
+    for k, v in config_kwargs.items():
+        if isinstance(v, str):
+            if v.lower() == "true":
+                config_kwargs[k] = True
+            elif v.lower() == "false":
+                config_kwargs[k] = False
+            elif v.isnumeric():
+                config_kwargs[k] = float(v)
+            elif re.match(r"^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$", v):
+                config_kwargs[k] = float(v)
+
+    config = config_kwargs
+    return config
+
+
 class NpEncoder(json.JSONEncoder):
     # json format for saving numpy array
     def default(self, obj):
