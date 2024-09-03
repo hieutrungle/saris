a02648f684e457d4772bfda7040d2806eb0aca96
HieuLe <hieult1996@gmail.com>
2024-09-03 10:42:10 -0500

    tmp dir

--- run_drl_L_hallway_poetry_1.sh
+++ run_drl_L_hallway_poetry_1.sh
@@ -72,7 +72,7 @@ export ASSETS_DIR
 export SIONNA_CONFIG_FILE
 export DRL_CONFIG_FILE
 export TMP_DIR
-export OPTIX_CACHE_PATH=${SOURCE_DIR}/tmp/optix_cache_1
+export OPTIX_CACHE_PATH=${TMP_DIR}/optix_cache_1
 mkdir -p ${OPTIX_CACHE_PATH}
 
 ##############################
@@ -80,5 +80,5 @@ mkdir -p ${OPTIX_CACHE_PATH}
 ##############################
 poetry run main --command train -dcfg ${DRL_CONFIG_FILE} -scfg ${SIONNA_CONFIG_FILE} -v --resume
 
-export OPTIX_CACHE_PATH=${SOURCE_DIR}/tmp/optix_cache_1
+export OPTIX_CACHE_PATH=${TMP_DIR}/optix_cache_1
 poetry run main --command eval -dcfg ${DRL_CONFIG_FILE} -scfg ${SIONNA_CONFIG_FILE} -v
