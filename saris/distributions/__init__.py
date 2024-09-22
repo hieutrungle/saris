577b4fbf564f97c56160ce9cb1e580675545ba96
hieule <hieult1996@gmail.com>
2024-09-22 12:06:35 -0500

    remove distribution for jax and create focal_pts exp to visualize focusing

--- saris/distributions/__init__.py
+++
@@ -1,6 +0,0 @@
-from saris.distributions.normal import Normal
-from saris.distributions.distribution import Distribution
-from saris.distributions.tanh import Tanh
-from saris.distributions.bijector import Bijector
-from saris.distributions.transformed import Transformed
-from saris.distributions.independent import Independent
