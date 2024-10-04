1a878ab22f07bb590b36aa7ab4b46215648168e0
HieuLe <hieult1996@gmail.com>
2024-10-03 23:33:08 -0500

    control hex in groups

--- saris/blender_script/bl_drl.py
+++ saris/blender_script/bl_drl.py
@@ -18,7 +18,73 @@ import bl_utils, bl_parser, shared_utils
 def export_drl_hallway_hex(args):
 
     # unit: degrees
-    theta_config, phi_config, num_groups = shared_utils.get_reflector_config()
+    theta_config, phi_config, num_groups, num_elements_per_group = (
+        shared_utils.get_reflector_config()
+    )
+    theta_range = (theta_config[1], theta_config[2])
+    phi_range = (phi_config[1], phi_config[2])
+
+    devices_names = []
+    object_dict = {f"Group{i:02d}": [] for i in range(1, num_groups + 1)}
+
+    for k, v in bpy.data.collections.items():
+        if "Reflector" in k:
+            devices_names.append(k)
+            sorted_objects = sorted(v.objects, key=lambda x: x.name)
+            for obj in sorted_objects:
+                concat_name = obj.name.strip().split(".")
+                group_name = concat_name[0]
+                object_dict[group_name].append(obj)
+
+    with open(args.input_path, "rb") as f:
+        # data: Tuple[np.ndarray[float], np.ndarray[float]]
+        spherical_focal_vecs = pickle.load(f)
+    spherical_focal_vecs = spherical_focal_vecs.reshape(num_groups, 3)
+
+    # Angle container
+    angles = [0.0 for _ in range(num_groups * (num_elements_per_group + 1))]
+
+    for i, (group_name, objects) in enumerate(object_dict.items()):
+        mid_tile = objects[num_elements_per_group // 2]
+        r_mid, theta_mid, phi_mid = spherical_focal_vecs[i]
+        focal_vec = bl_utils.spherical2cartesian(r_mid, theta_mid, phi_mid)
+        focal_pt = bl_utils.get_center_bbox(mid_tile) + Vector(focal_vec)
+        theta_mid = shared_utils.constraint_angle(theta_mid, theta_range)
+        angles[i * (num_elements_per_group + 1)] = theta_mid
+
+        for j, obj in enumerate(objects):
+            center = bl_utils.get_center_bbox(obj)
+            r, theta, phi = bl_utils.compute_rot_angle(center, focal_pt)
+            theta = shared_utils.constraint_angle(theta, theta_range)
+            phi = shared_utils.constraint_angle(phi, phi_range)
+            obj.rotation_euler = [0, phi, theta]
+            angles[i * (num_elements_per_group + 1) + j + 1] = phi
+
+    result_path = args.input_path
+    with open(result_path, "wb") as f:
+        pickle.dump(angles, f)
+
+    # Save files without ceiling
+    folder_dir = os.path.join(args.output_dir, f"idx")
+    bl_utils.mkdir_with_replacement(folder_dir)
+    bl_utils.save_mitsuba_xml(folder_dir, "hallway", [*devices_names, "Wall", "Floor"])
+
+    # Save files with ceiling
+    folder_dir = os.path.join(args.output_dir, f"ceiling_idx")
+    bl_utils.mkdir_with_replacement(folder_dir)
+    bl_utils.save_mitsuba_xml(
+        folder_dir,
+        "hallway",
+        [*devices_names, "Wall", "Floor", "Ceiling"],
+    )
+
+
+def tmp_export_drl_hallway_hex(args):
+
+    # unit: degrees
+    theta_config, phi_config, num_groups, num_elements_per_group = (
+        shared_utils.get_reflector_config()
+    )
     theta_range = (math.radians(theta_config[1]), math.radians(theta_config[2]))
     phi_range = (math.radians((phi_config[1])), math.radians(phi_config[2]))
 
--- saris/blender_script/bl_utils.py
+++ saris/blender_script/bl_utils.py
@@ -102,28 +102,28 @@ def save_mitsuba_xml(folder_dir, filename, collections):
     )
 
 
-def get_bisector_pt(pt1: Vector, pt2: Vector, pt3: Vector) -> Vector:
-    """
-    Get the bisector point of the line between tx and rx.
+# def get_bisector_pt(pt1: Vector, pt2: Vector, pt3: Vector) -> Vector:
+#     """
+#     Get the bisector point of the line between tx and rx.
 
-    pt1: B
-    pt2: A
-    pt3: C
-    bisector_pt: D
+#     pt1: B
+#     pt2: A
+#     pt3: C
+#     bisector_pt: D
 
-    ratio = -|AB|/|AC|
+#     ratio = -|AB|/|AC|
 
-    vec(BD) = ratio * vec(CD)
+#     vec(BD) = ratio * vec(CD)
 
-    x_D = 1/(1 - ratio) * (x_B - ratio * x_C) \n
-    y_D = 1/(1 - ratio) * (y_B - ratio * y_C) \n
-    z_D = 1/(1 - ratio) * (z_B - ratio * z_C)
-    """
-    tile_tx_len = (pt1 - pt2).length  # |AB|
-    tile_rx_len = (pt3 - pt2).length  # |AC|
-    ratio = -tile_tx_len / tile_rx_len
-    bisector_pt = 1 / (1 - ratio) * (pt1 - ratio * pt3)
-    return bisector_pt
+#     x_D = 1/(1 - ratio) * (x_B - ratio * x_C) \n
+#     y_D = 1/(1 - ratio) * (y_B - ratio * y_C) \n
+#     z_D = 1/(1 - ratio) * (z_B - ratio * z_C)
+#     """
+#     tile_tx_len = (pt1 - pt2).length  # |AB|
+#     tile_rx_len = (pt3 - pt2).length  # |AC|
+#     ratio = -tile_tx_len / tile_rx_len
+#     bisector_pt = 1 / (1 - ratio) * (pt1 - ratio * pt3)
+#     return bisector_pt
 
 
 def get_center_bbox(tile: bpy.types.Object) -> Vector:
@@ -133,45 +133,68 @@ def get_center_bbox(tile: bpy.types.Object) -> Vector:
     return global_bbox_center
 
 
-def get_midpoint(pt1, pt2):
-    x = (pt1[0] + pt2[0]) / 2
-    y = (pt1[1] + pt2[1]) / 2
-    z = (pt1[2] + pt2[2]) / 2
-    return [x, y, z]
+# def get_midpoint(pt1, pt2):
+#     x = (pt1[0] + pt2[0]) / 2
+#     y = (pt1[1] + pt2[1]) / 2
+#     z = (pt1[2] + pt2[2]) / 2
+#     return [x, y, z]
 
 
-def compute_rot_angle_3pts(
-    pt1: list,
-    pt2: list,
-    pt3: list,
-) -> Tuple[float, float, float]:
-    """Compute the rotation angles for the pt2.
-    The three point forms a triangle, we compute the rotation angles for the pt2.
-    return: (r, theta, phi)
-        `r`: distance from the tile center to the midpoint of tx and rx
-        `theta`: rotation in y-axis
-        `phi`: rotation in z-axis
-    """
-    midpoint = get_bisector_pt(Vector(pt1), Vector(pt2), Vector(pt3))
-    return compute_rot_angle(pt2, midpoint)
+# def compute_rot_angle_3pts(
+#     pt1: list,
+#     pt2: list,
+#     pt3: list,
+# ) -> Tuple[float, float, float]:
+#     """Compute the rotation angles for the pt2.
+#     The three point forms a triangle, we compute the rotation angles for the pt2.
+#     return: (r, theta, phi)
+#         `r`: distance from the tile center to the midpoint of tx and rx
+#         `theta`: rotation in y-axis
+#         `phi`: rotation in z-axis
+#     """
+#     midpoint = get_bisector_pt(Vector(pt1), Vector(pt2), Vector(pt3))
+#     return compute_rot_angle(pt2, midpoint)
 
 
-def compute_rot_angle(
-    tile_center: list,
-    pt: list,
-) -> Tuple[float, float, float]:
-    """Compute the rotation angles for the tile.
-    return: (r, theta, phi)
-        `r`: distance from the tile center to a point
-        `theta`: rotation in y-axis
-        `phi`: rotation in z-axis
-    """
-    x = tile_center[0] - pt[0]
-    y = tile_center[1] - pt[1]
-    z = tile_center[2] - pt[2]
+# def compute_rot_angle(
+#     tile_center: list,
+#     pt: list,
+# ) -> Tuple[float, float, float]:
+#     """Compute the rotation angles for the tile.
+#     return: (r, theta, phi)
+#         `r`: distance from the tile center to a point
+#         `theta`: rotation in y-axis
+#         `phi`: rotation in z-axis
+#     """
+#     x = tile_center[0] - pt[0]
+#     y = tile_center[1] - pt[1]
+#     z = tile_center[2] - pt[2]
 
+#     r = math.sqrt(x**2 + y**2 + z**2)
+#     theta = math.acos(z / r)  # rotation in y-axis
+#     phi = sign(y) * math.acos(x / math.sqrt(x**2 + y**2))  # rotation in z-axis
+
+#     return (r, theta, phi)
+
+
+def compute_rot_angle(pt1: list, pt2: list) -> Tuple[float, float, float]:
+    """Compute the rotation angles for vector pt1 to pt2."""
+    x = pt2[0] - pt1[0]
+    y = pt2[1] - pt1[1]
+    z = pt2[2] - pt1[2]
+
+    return cartesian2spherical(x, y, z)
+
+
+def cartesian2spherical(x: float, y: float, z: float) -> Tuple[float, float, float]:
     r = math.sqrt(x**2 + y**2 + z**2)
-    theta = math.acos(z / r)  # rotation in y-axis
-    phi = sign(y) * math.acos(x / math.sqrt(x**2 + y**2))  # rotation in z-axis
+    theta = math.atan2(y, x)
+    phi = math.acos(z / r)
+    return r, theta, phi
 
-    return (r, theta, phi)
+
+def spherical2cartesian(r: float, theta: float, phi: float) -> Tuple[float, float, float]:
+    x = r * math.sin(phi) * math.cos(theta)
+    y = r * math.sin(phi) * math.sin(theta)
+    z = r * math.cos(phi)
+    return x, y, z
