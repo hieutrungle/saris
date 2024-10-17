7e83b414066604983ad076ed08a2547490d7cd40
HieuLe <hieult1996@gmail.com>
2024-10-17 17:18:00 -0500

    train longer with more updates, reduce discount factor

--- saris/run_sac_torchcompile.py
+++ saris/run_sac_torchcompile.py
@@ -28,17 +28,17 @@ class TrainConfig:
     # Environment specific arguments
     env_id: str = "wireless-sigmap-v0"  # the environment id of the task
     sionna_config_file: str = "-1"  # Sionna config file
-    num_envs: int = 8  # the number of parallel environments
+    num_envs: int = 10  # the number of parallel environments
     ep_len: int = 75  # the maximum length of an episode
     eval_ep_len: int = 75  # the maximum length of an episode
 
     # Algorithm specific arguments
-    total_timesteps: int = 50_001  # total timesteps of the experiments
-    n_updates: int = 10  # the number of updates per step
-    buffer_size: int = int(100_000)  # the replay memory buffer size
-    gamma: float = 0.85  # the discount factor gamma
+    total_timesteps: int = 20_001  # total timesteps of the experiments
+    n_updates: int = 35  # the number of updates per step
+    buffer_size: int = int(150_000)  # the replay memory buffer size
+    gamma: float = 0.75  # the discount factor gamma
     tau: float = 0.005  # target smoothing coefficient (default: 0.005)
-    batch_size: int = 128  # the batch size of sample from the reply memory
+    batch_size: int = 256  # the batch size of sample from the reply memory
     learning_starts: int = 601  # the timestep to start learning
     policy_lr: float = 3e-4  # the learning rate of the policy network optimizer
     q_lr: float = 1e-3  # the learning rate of the q network optimizer
