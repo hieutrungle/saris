e71a4880daf6f393069ead74b9a1f47485af156b
hieule <hieult1996@gmail.com>
2024-10-18 10:20:49 -0500

    scale down gain_diff

--- saris/drl/envs/wireless/wireless_v0.py
+++ saris/drl/envs/wireless/wireless_v0.py
@@ -211,7 +211,7 @@ class WirelessEnvV0(Env):
         lower_ = -100.0
         upper_ = -80.0
 
-        reward = total_gain + 0.4 * gain_diff - 0.05 * cost_time
+        reward = total_gain + 0.3 * gain_diff - 0.05 * cost_time
         reward = (reward - lower_) / (upper_ - lower_)
 
         return float(reward)
