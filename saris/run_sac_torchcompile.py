50bf71f450e29c91eee28ad8ffa1749e54b75d38
hieule <hieult1996@gmail.com>
2024-10-27 17:00:13 -0500

    small replay_buffer

--- saris/run_sac_torchcompile.py
+++ saris/run_sac_torchcompile.py
@@ -34,7 +34,7 @@ class TrainConfig:
     # Algorithm specific arguments
     total_timesteps: int = 6_001  # total timesteps of the experiments
     n_updates: int = 20  # the number of updates per step
-    buffer_size: int = int(80_000)  # the replay memory buffer size
+    buffer_size: int = int(40_000)  # the replay memory buffer size
     gamma: float = 0.9  # the discount factor gamma
     tau: float = 0.005  # target smoothing coefficient (default: 0.005)
     batch_size: int = 256  # the batch size of sample from the reply memory
