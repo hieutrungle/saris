dacb60f0c92d7e876a80f497e60203170b883d79
hieule <hieult1996@gmail.com>
2024-11-12 20:09:14 -0600

    update rms channel at learning_starts

--- saris/train_sac.py
+++ saris/train_sac.py
@@ -591,17 +591,18 @@ def train_agent(
             actions = policy(normalized_flat_obs)
             actions = actions.cpu().numpy()
 
-        # TRY NOT TO MODIFY: execute the game and log data.
-        try:
-            next_obs, rewards, terminations, truncations, infos = envs.step(actions)
-        except Exception as e:
-            traceback.print_exc()
+        if global_step == config.learning_starts and len(stored_flat_obs) > 0:
             # update channel rms normalization
             stored_flat_obs = np.concatenate(stored_flat_obs, axis=0)
             update_channel_rmss(torch.tensor(stored_flat_obs), obs_rmss[0], obs_rmss[1])
             torch.save({"obs_rmss": obs_rmss}, os.path.join(config.checkpoint_dir, "obs_rmss.pth"))
             stored_flat_obs = []
-            # Reset envs
+
+        # TRY NOT TO MODIFY: execute the game and log data.
+        try:
+            next_obs, rewards, terminations, truncations, infos = envs.step(actions)
+        except Exception as e:
+            traceback.print_exc()
             obs, _ = envs.reset(seed=config.seed)
             flat_obs = np.concatenate([ob.reshape(ob.shape[0], -1) for ob in obs], axis=-1)
             continue
