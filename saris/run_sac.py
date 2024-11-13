c6d726ce61a10f6ccff6dfca16f05ddd33219c9d
hieule <hieult1996@gmail.com>
2024-11-13 16:32:15 -0600

    learning start at 201

--- saris/run_sac.py
+++ saris/run_sac.py
@@ -41,7 +41,7 @@ class TrainConfig:
     gamma: float = 0.99  # the discount factor gamma
     tau: float = 0.005  # target smoothing coefficient (default: 0.005)
     batch_size: int = 128  # the batch size of sample from the reply memory
-    learning_starts: int = 101  # the timestep to start learning
+    learning_starts: int = 201  # the timestep to start learning
     policy_lr: float = 3e-4  # the learning rate of the policy network optimizer
     q_lr: float = 1e-3  # the learning rate of the q network optimizer
     warmup_steps: int = 300  # the number of warmup steps
