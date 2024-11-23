a85323c9aa84b9a9b6fc7ab37f22fb8130247f38
HieuLe <hieult1996@gmail.com>
2024-11-22 19:42:23 -0600

    train per 2000 steps

--- saris/run_sac.py
+++ saris/run_sac.py
@@ -35,7 +35,7 @@ class TrainConfig:
     eval_ep_len: int = 100  # the maximum length of an episode
 
     # Algorithm specific arguments
-    total_timesteps: int = 2_501  # total timesteps of the experiments
+    total_timesteps: int = 2_001  # total timesteps of the experiments
     n_updates: int = 5  # the number of updates per step
     buffer_size: int = int(40_000)  # the replay memory buffer size
     gamma: float = 0.985  # the discount factor gamma
