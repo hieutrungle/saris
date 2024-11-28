edf0db42ca268ff66247c8447a9b20c2991cb464
HieuLe <hieult1996@gmail.com>
2024-11-28 13:41:48 -0600

    reward

--- saris/drl/envs/wireless_moving/wireless_moving_v0.py
+++ saris/drl/envs/wireless_moving/wireless_moving_v0.py
@@ -280,9 +280,9 @@ class WirelessMovingV0(Env):
     ) -> float:
 
         adjusted_gains = np.where(
-            cur_gains < -80,
-            (cur_gains + 80) / 20,
-            np.log(1 + 80 + cur_gains) * 2 + 1.0,
+            cur_gains < -82.5,
+            (cur_gains + 82.5) / 20,
+            np.log(1 + 82.5 + cur_gains) * 2 + 1.0,
         )
         adjusted_gain = np.mean(adjusted_gains)
         gain_diff = np.mean(next_gains - cur_gains)
@@ -298,7 +298,7 @@ class WirelessMovingV0(Env):
 
         # # gain_diff = np.mean(next_gains - cur_gains)
 
-        reward = float(adjusted_gain + 0.03 * gain_diff - 0.5 * out_of_bounds) / 2.0
+        reward = float(adjusted_gain + 0.03 * gain_diff - 0.3 * out_of_bounds) / 2.0
 
         # print(f"mean_gain: {mean_gain}, adjusted_gain: {adjusted_gain}, reward: {reward}")
 
