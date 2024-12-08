bbe46ad9ab16c2698911df6588b8da0703b6ffd6
HieuLe <hieult1996@gmail.com>
2024-12-07 21:08:05 -0600

    run with long_short_NN

--- run_room_sac_static.sh
+++ run_room_sac_static.sh
@@ -79,5 +79,5 @@ mkdir -p ${OPTIX_CACHE_PATH}
 # DRL run
 ##############################
 export OPTIX_CACHE_PATH=${TMP_DIR}/optix_cache_1
-python ./saris/run_sac.py --sionna_config_file ${SIONNA_CONFIG_FILE} --verbose True --env_id "single-room-v0" --group "SAC_ROOM_Static" --name "short_long_NN" 
+python ./saris/run_sac.py --sionna_config_file ${SIONNA_CONFIG_FILE} --verbose True --env_id "single-room-v0" --group "SAC_ROOM_Static" --name "long_short_NN" 
 # --seed 22 --num_envs 2 --total_timesteps 15 --batch_size 4 --learning_starts 0 --ep_len 10 --save_interval 25 --eval_ep_len 10 --wandb "offline"  --init_learning_start 5 --n_runs 2
